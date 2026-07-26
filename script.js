8652c9c3c25a0fff59c3da845599b406c55d165f
yousseffaizbs-create <yousseffaiz.bs@gmail.com>
2026-07-26 23:18:52 +0100

    Refactor script.js by removing button listener and adding styles
    Removed button click event listener and added CSS styles for body, headings, input, and task list.

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-const button =
-document.querySelector("button");
-button.addEventListener("click",
-function () {
-    alert("I love you so so so so so much");
-});
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    text-align: center;
+    margin-top: 50px;
+}
+h1 {
+    color: #333;
+}
+input {
+    padding: 10px;
+    width: 250px;
+}
+button {
+    padding: 10px 15 px;
+    cursor: pointer;
+}
+#tasklist {
+    text-align: left;
+    width: 300px;
+    margin: 20px auto;
+}
+#tasklist Li{
+    margin-bottom: 15px;
+    text-align: left;
+    list-style-position: outside;
+    cursor: pointer;
+}
+#tasklist button {
+    margin-left: 10px;
+    padding: 5px 10px;
+    cursor: pointer;
+}
+.done {
+    text-decoration: line-through;
+    color :gray;
+}
